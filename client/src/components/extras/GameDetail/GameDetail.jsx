a2be19eff7300bb36cac4a093c99354d83101723
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2023-12-11 13:44:33 -0300

    Cambios a textos

--- client/src/components/extras/GameDetail/GameDetail.jsx
+++ client/src/components/extras/GameDetail/GameDetail.jsx
@@ -1,4 +1,11 @@
-import { answer, ask, conquestAndDeffend, conquistaYDefiende, pregunta, respuesta, invocaTropas, summonTroops, invadeMapas, invadeMaps, mejoraText, updatesText} from "../../../data/docs/countlessArmy"
+import { 
+    answer, ask, 
+    conquestAndDeffend, conquistaYDefiende, 
+    pregunta, respuesta, 
+    invocaTropas, summonTroops,
+    invadeMapas, invadeMaps, 
+    mejoraText, updatesText, 
+    requisitosEn, requisitosEs, sisRequirements, sistRequisitos} from "../../../data/docs/countlessArmy"
 import { gamesImg } from "../../../data/img"
 import { conquest, conquista, conquistaEdif } from "../../../data/img/conquista"
 import { invasion, invation, mapas } from "../../../data/img/invasion/invation"
@@ -7,44 +14,51 @@ import { mapasEn, mapaGif, mapasEs } from "../../../data/img/mapa"
 const GameDetail = ({language})=>{
 
     return(
-        <div>
+        <section>
             <img src={gamesImg[1]} style={{marginTop:'1em'}}  alt="game banner" />
             <p style={{color:'white'}}>{language === 'ES' ? pregunta : ask}</p>
             <p style={{color:'white'}}>{language === 'ES' ? respuesta : answer}</p>
-            <div style={{display:"flex", flexDirection:'column', marginLeft:'25%', marginRight:'25%'}}>
+            <section style={{display:"flex", flexDirection:'column', marginLeft:'25%', marginRight:'25%'}}>
                 {language === 'ES' ? <img src={conquista} alt='conquista' />: <img src={conquest} alt='conquest' />}
-                <div style={{display:"flex", flexDirection:"row"}}>
-                    <p style={{color:'white', marginTop:'15%', marginRight:'.5em'}}>{language === 'ES' ? invocaTropas : summonTroops}</p>
+                <section style={{display:"flex", flexDirection:"row"}}>
+                    <div style={{color:'whitesmoke', marginRight:'0.75em'}}>{language === 'ES' ? invocaTropas.map(texto=><p>{texto}</p>) : summonTroops}</div>
                     <img src={conquistaEdif} style={{width:'30rem'}} alt="conquer buildings" />
-                </div>
-            </div>
-            <div style={{display:"flex", flexDirection:'column', marginLeft:'25%', marginRight:'25%'}}>
+                </section>
+            </section>
+            <section style={{display:"flex", flexDirection:'column', marginLeft:'25%', marginRight:'25%'}}>
                 {language === 'ES' ? <img src={invasion} alt="Invade mapas" />: <img src={invation} alt="Invade maps" />}
-                <div style={{display:"flex", flexDirection:"row"}}>
-                    <p style={{color:'white', marginTop:'15%', marginRight:'.5em'}}>{language === 'ES' ? invadeMapas : invadeMaps}</p>
+                <section style={{display:"flex", flexDirection:"row"}}>
+                    <div style={{color:'white', marginTop:'0.25em', marginRight:'.5em'}}>{language === 'ES' ? invadeMapas.map(texto=> <p>{texto}</p>) : invadeMaps}</div>
                     <img src={mapas} style={{width:'30rem'}} alt="invade maps" />
-                </div>
-            </div>
-            <div style={{display:"flex", flexDirection:'column', marginLeft:'25%', marginRight:'25%'}}>
+                </section>
+            </section>
+            <section style={{display:"flex", flexDirection:'column', marginLeft:'25%', marginRight:'25%'}}>
                 {language === 'ES' ? <img src={mejoras} alt="Mejora y actualiza" />: <img src={updates} alt="Updates and unlocks" />}
-                <div style={{display:"flex", flexDirection:"row"}}>
+                <section style={{display:"flex", flexDirection:"row"}}>
                     <p style={{color:'white', marginTop:'15%', marginRight:'.5em'}}>{language === 'ES' ? mejoraText : updatesText}</p>
                     <img src={mejorasGif} style={{width:'30rem'}} alt="update and unlock" />
-                </div>
-            </div>
+                </section>
+            </section>
             {/* conquista */}
-            <div style={{display:"flex", flexDirection:'column', marginLeft:'25%', marginRight:'25%'}}>
+            <section style={{display:"flex", flexDirection:'column', marginLeft:'25%', marginRight:'25%'}}>
                 {language === 'ES' ? <img src={mapasEs} alt="mapa esp" />: <img src={mapasEn} alt="map En" />}
-                <div style={{display:"flex", flexDirection:"row"}}>
+                <section style={{display:"flex", flexDirection:"row"}}>
                     <p style={{color:'white', marginTop:'15%', marginRight:'.5em'}}>{language === 'ES' ? conquistaYDefiende : conquestAndDeffend}</p>
                     <img src={mapaGif} style={{width:'30rem'}} alt="defend and conquer map" />
-                </div>
-            </div>
+                </section>
+            </section>
+            <section style={{color:'white'}}>
+                {language === 'ES' ? <h1>{sistRequisitos}</h1> : <h1>{sisRequirements}</h1>}
+                <section>
+                    <ul style={{color:'white'}}>
+                        {language=== 'ES' ? requisitosEs.map((requisito)=><p>{requisito}</p>): requisitosEn.map((requisito)=><p>{requisito}</p>)}
+                    </ul>
+                </section>
+            </section>
 
 
 
-
-        </div>
+        </section>
     )
 }
 
--- client/src/data/docs/countlessArmy.js
+++ client/src/data/docs/countlessArmy.js
@@ -4,10 +4,10 @@ export const respuesta = 'Sé el que envía oleadas tras oleada de unidades para
 export const ask='Did you dreamed being the villain in a Tower Defense?'
 export const answer= 'Be the one that send wave after waver of troops to break the enemy defenses in this inverse Tower Defense'
 
-export const invocaTropas = 'Selecciona e invoca tus unidades preferidas para conquistar los diversos escenarios.'
+export const invocaTropas = ['Selecciona a tus tropas entre las 12 disponibles para conquistar cada mapa. Piensa bien tus elecciones, pues cada unidad tiene distintas características y habilidades.', '¡Establece tus estrategias y conquista el mundo entero!']
 export const summonTroops = 'Select and summon your favorites units to conquest diverse scenarios.'
 
-export const invadeMapas = 'Has tu travesia por diferentes mapas y conquistalos!'
+export const invadeMapas = ['Cada mapa es único, costas, bosques, montañas, llanuras, ciudades; distintos terrenos, caminos y defensas.', '¡Observa bien y establece tus estrategias acorde a ellos para conquistar cada uno de los 46 mapas en total!']
 export const invadeMaps = 'Make your journey trough different maps and conquer them'
 
 export const mejoraText= 'Desbloquea unidades y hazlas mas fuertes actualizandolas'
@@ -15,3 +15,26 @@ export const updatesText= 'Unlock units and make them stronger udating them'
 
 export const conquistaYDefiende = 'Conquista y defiende diferentes zonas del mapa'
 export const conquestAndDeffend = 'Conquer and deffend different zones of the map'
+
+export const sisRequirements= 'System requirements'
+export const sistRequisitos= 'Requisitos del sistema'
+export const requisitosEs= [
+'Mínimo:',
+'SO: Windows 7',
+'Procesador: Dual Core',
+'Memoria: 500 MB RAM',
+'Gráficos: ATI Radeon HD 4850 or NVIDIA GeForce 9600 GT',
+'DirectX: Versión 11',
+'Almacenamiento: 150 MB de espacio disponible',
+'Tarjeta de sonido: tarjeta de sonido compatible con DirectX'
+]
+export const requisitosEn= [
+    'Minimum:',
+    'SO: Windows 7',
+    'Processorr: Dual Core',
+    'Memory: 500 MB RAM',
+    'Graphics: ATI Radeon HD 4850 or NVIDIA GeForce 9600 GT',
+    'DirectX: Version 11',
+    'Storage: 150 MB available space',
+    'Sound card: DirectX compatible sound card'
+    ]
