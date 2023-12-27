f750d9c9f8c5839c58e22e0e19f69f5e54dd616b
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2023-12-27 13:32:15 -0300

    Contact data

--- client/src/data/docs/contactos.js
+++ client/src/data/docs/contactos.js
@@ -1,1 +1,7 @@
 export const mailEmpresa= 'dos.estrellas.games@gmail.com'
+
+export const presentacion=['Tienes alguna duda','Contactanos!']
+export const presentation=['You have a doubt?', 'Contact us!']
+
+export const sociales='También puedes visitar nustras redes sociales:'
+export const socials= 'Also you can visit our social networks: '
