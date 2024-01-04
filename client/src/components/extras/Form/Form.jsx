a5d771dacaedeb12313f4b969e2ed3d09dec68dc
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2024-01-04 17:58:56 -0300

    form sin estilos

--- client/src/components/extras/Form/Form.jsx
+++ client/src/components/extras/Form/Form.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react"
-import { PiHandEyeLight } from "react-icons/pi"
+import { useEffect, useState } from "react"
+import { Validation } from "./Validation"
 
 const Form = ({language})=>{
 
     const [gameRelated, setGameRelated]= useState(false)
+    const [disable, setDisable]=useState(true)
+    const [selectedGame, setSelectedGame]=useState('')
+
     const [inputs, setInputs]=useState({
-        autor:'',
+        email:'',
         tittle:'',
         message:''
     })
     
     const [error, setError]=useState({
-        name:'',
+        email:'',
         tittle:'',
         message:''
     })
@@ -27,30 +30,86 @@ const Form = ({language})=>{
     const handleSelect1= (event)=>{
         const selectedValue=event.target.value
         selection(selectedValue)
+
+        setInputs({
+            ...inputs,
+            tittle: selectedValue !== 'None' ? selectedValue : ''
+        })
     }
+
+    const handleSelect2= (event)=>{
+        const selectedGameValue = event.target.value;
+        setSelectedGame(selectedGameValue);
+        setInputs({
+            ...inputs,
+            tittle: `${selectedGameValue !== 'None' ? selectedGameValue + ' - ' : ''}${inputs.tittle}`,
+        });
+
+        setDisable(!(inputs.email && inputs.message && inputs.tittle));
+
+    }
+    
+    const handleOnChange = (event) => {
+        const { name, value } = event.target;
+        setInputs({
+            ...inputs,
+            [name]: value,
+        });
+
+        setError((prevError) => ({
+            ...prevError,
+            [name]: Validation({ ...inputs, [name]: value }, language)[name],
+        }));
+
+        setDisable(!(inputs.email && inputs.message && inputs.tittle));
+    };
+
+    const handleSubmit = (event)=>{
+        try {
+            if(error.email || error.message || error.tittle){
+                if(language === 'ES'){
+                    alert('Tienes algo equivocado')
+                }else{
+                    alert('You have something wrong')
+                }
+            }
+        } catch (error) {
+            console.error('Error en la solicitud', error)
+        }
+        event.preventDefault();
+    }
+
     return(
-        <form action="" style={{color:'white', display:"flex", flexDirection:'column', width:'20em', marginLeft:'39.5%'}}>
-            <label htmlFor="">Email</label>
-            <input type="text" />
+        <form onSubmit={handleSubmit} style={{color:'white', display:"flex", flexDirection:'column', width:'20em', marginLeft:'39.5%'}}>
+            <label htmlFor="Email">Email</label>
+            <input onChange={handleOnChange} type="text" name='email' value={inputs.autor} />
+            <p>{error.email}</p>
             <section>
                 <label htmlFor="">{language === 'ES' ? 'Categoria':'Category'}</label>
-                <select name="" id="" onChange={handleSelect1}>
+                <select name="Categorias" id="CatSelect" onChange={handleSelect1}>
                         <option value="None"> - - - </option>
                         <option value="Problemas con un juego">Problemas con un juego</option>
                         <option value="Se encontro un bug">Encontre un bug</option>
                         <option value="Contacto comercial">Contacto comercial</option>
                 </select>
                 {gameRelated && 
-                <select name="" id="">
-                    <option value="None">- - -</option>
-                    <option value="Countless Army">Countless Army</option>
-                </select>
+                    <section>
+                        <label htmlFor="Juego">Juego</label>
+                        <select onChange={handleSelect2} name="Select2" id="GameSelect">
+                            <option value="">- - -</option>
+                            <option value="Countless Army">Countless Army</option>
+                        </select>
+                </section>
                 }
             </section>
-            <label htmlFor="">Asunto</label>
-            <input type="text" />
-            <label htmlFor="">Mensaje:</label>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <label htmlFor="Titulo">Asunto</label>
+            <input onChange={handleOnChange} type="text" value={inputs.tittle} name='tittle' />
+            <p>{error.tittle}</p>
+            <label htmlFor="Mensaje">Mensaje:</label>
+            <textarea onChange={handleOnChange} name="message" value={inputs.message} id="" cols="30" rows="10"></textarea>
+            <p>{inputs.message.length}/500</p>
+            <p>{error.message}</p>
+            <button type="submit" disabled={disable} style={{width:'fit-content'}} > Enviar </button>
         </form>
     )
 }
---
+++ client/src/components/extras/Form/Validation.js
@@ -0,0 +1,88 @@
+const emailVal=/^([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,6})$/
+export const Validation=(inputs, language)=>{
+
+    const errors ={}
+
+    //  Email
+    switch(true){
+        case !inputs.email:
+            if(language === 'ES'){
+                errors.email='Este campo es obligatorio'
+            }else{
+                errors.email='This field is obligatory'
+            }
+            break;
+        case inputs.email.length >40:
+            if(language === 'ES'){
+                errors.email='El email es muy largo'
+            }else{
+                errors.email='The email is too long'
+            }
+            break;
+        case !emailVal.test(inputs.email):
+            if(language === 'ES'){
+                errors.email='Este no es un email valido'
+            }else{
+                errors.email="This isn't a valid email"
+            }
+            break;
+        default:
+            errors.email=''
+    }
+
+    //  Asunto
+    switch(true){
+        case !inputs.tittle:
+            if(language=== 'ES'){
+                errors.tittle='Debes ingresar un asunto'
+            }else{
+                errors.tittle='You need a subject'
+            }
+            break;
+        case inputs.tittle.length <5:
+            if(language === 'ES'){
+                errors.tittle='El asunto es demasiado corto'
+            }else{
+                errors.tittle='The subject is too short'
+            }
+            break;
+        case inputs.tittle.length >35:
+            if(language === 'ES'){
+                errors.tittle='El asunto es muy largo'
+            }else{
+                errors.tittle='The subject is too long'
+            }
+            break;
+        default:
+            errors.tittle=''
+    }
+    //  Mensaje
+    switch(true){
+        case !inputs.message:
+            if (language === 'ES'){
+                errors.message='Es necesario que escriba un mensaje'
+            }else{
+                errors.message='You need to write a message'
+            }
+            break
+        case inputs.message.length > 500:
+            if (language === 'ES'){
+                errors.message='Tu mensaje es demasiado largo'
+            }else{
+                errors.message='Yout message is too large'
+            }
+            break
+        case inputs.message.length < 50:
+            if (language === 'ES'){
+                errors.message='Tu mensaje debe tener al menos 50 caracteres'
+            }else{
+                errors.message='Your message must have at least 50 characters'
+            }
+            break
+        default:
+            errors.message=''
+    }
+/* texto = 500 caracteres */
+    return errors
+    
+}
