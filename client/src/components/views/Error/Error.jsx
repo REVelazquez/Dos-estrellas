aa5ad7bf44836f33e6fe390e85fe3f37c640b0d6
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2024-01-12 23:46:35 -0300

    Style changes

--- client/src/components/views/Error/Error.jsx
+++ client/src/components/views/Error/Error.jsx
@@ -3,7 +3,7 @@ import { errorPage } from "../../../data/img"
 const Error = ()=>{
     const navigate =useNavigate()
     return (
-        <div style={{minHeight:'31em'}}>
+        <div style={{minHeight:'75vh'}}>
             <img style={{marginTop:'5em'}} src={errorPage} alt="" />
             <h1 style={{color:'yellow'}}>U shouldn't be here</h1>
             <button onClick={()=>navigate('/')} >Go Home</button>
