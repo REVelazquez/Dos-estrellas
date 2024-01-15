1c0a38f309c5f137c6802032ba8dfae44fb63fb9
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2024-01-15 15:34:47 -0300

    About + styles

--- client/src/components/views/About/About.jsx
+++ client/src/components/views/About/About.jsx
@@ -1,6 +1,11 @@
-const About = ()=>{
+import { presentacionEn, presentacionEs } from "../../../data/docs/about"
+import Style from './About.module.css'
 
-    return(<div>HOLI Falta info aca</div>)
+const About = ({language})=>{
+
+    return(<div key='aboutCont' className={Style.container}>
+        <h4 >{language === 'ES' ? presentacionEs.map(texto => <p>{texto}</p>) : presentacionEn.map(texto => <p>{texto}</p>) }</h4>
+    </div>)
 }
 
 export default About
---
+++ client/src/data/docs/about.js
@@ -0,0 +1,5 @@
+export const presentacionEs= ['Dos Estrellas nace como idea justo a finales de 2022 bajo las manos de dos amantes de los videojuegos obsesionados con plasmar sus disparatados conceptos, adquiridos durante muchas (créeme, muchas) horas de juego, a la realidad.',
+
+'A día de hoy trabajamos y luchamos por sacar a delante este sueño que siempre hemos tenido, poniendo todos nuestros esfuerzos, dedicación y cariño a nuestras creaciones.']
+
+export const presentacionEn=['Dos Estrellas was born as an idea right at the end of 2022 under the hands of two video game lovers obsessed with bringing their crazy concepts, acquired through many (believe me, MANY) hours of gaming, to reality.', 'Today, we are working and fighting to bring forward this dream we have always had, putting all our efforts, dedication, and love into our creations.']
