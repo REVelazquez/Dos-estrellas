27e6905f55972ee5ab2211007c75099da1e769b5
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2024-01-16 13:54:11 -0300

    styles

--- client/src/components/extras/GamesCarrousel/GamesCarrousel.jsx
+++ client/src/components/extras/GamesCarrousel/GamesCarrousel.jsx
@@ -10,15 +10,17 @@ const GamesCarrousel = ({language}) => {
             {currentElements.map(item => (
                 <section key={'Game='+ item.name} className={Style.container} >
                     <img className={Style.image} src={item.image} alt={item.name} />
-                    <section>
+                    <section className={Style.texts}>
                     <article>
-                        <h2 className={Style.tittle}>{language === 'ES' ? 'Genero' : 'Genre' }</h2>
+                        <h2 className={Style.tittle}>{language === 'ES' ? 'Genero:' : 'Genre:' }</h2>
                         <h3 className={Style.genre} >{language === 'ES' ? item.genero : item.genre}</h3>
                     </article>
-                    <button>{language === 'ES' ? 'Ver mas..' : 'See more...'}</button>
-                    <a href={item.steamPage} target="_blank" rel="noopener noreferrer">
-                        <FaSteam />
-                    </a>
+                    <section>
+                        <button className={Style.button}>{language === 'ES' ? 'Ver más..' : 'See more...'}</button>
+                        <a className={Style.steam} href={item.steamPage} target="_blank" rel="noopener noreferrer">
+                            <FaSteam />
+                        </a>
+                    </section>
 
                     </section>
                 </section>
