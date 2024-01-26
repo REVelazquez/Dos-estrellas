2668e0ea8079e7ae9fc536eea14cf001edeead83
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2024-01-26 17:49:40 -0300

    Changes to About, styles in the same comp

--- client/src/components/views/About/About.jsx
+++ client/src/components/views/About/About.jsx
@@ -1,10 +1,42 @@
-import { presentacionEn, presentacionEs } from "../../../data/docs/about"
+import { about } from "../../../data/docs/about"
 import Style from './About.module.css'
 
 const About = ({language})=>{
 
     return(<div key='aboutCont' className={Style.container}>
-        {language === 'ES' ? presentacionEs.map(texto => <h4 className={Style.presentation}>{texto}</h4>) : presentacionEn.map(texto => <h4 className={Style.presentation}>{texto}</h4>) }
+        <h2 className={Style.tittle}>Dos estrellas</h2>
+        {language === 'ES' ? about.presentacionEs.map(texto => <h4 className={Style.presentation}>{texto}</h4>) : about.presentacionEn.map(texto => <h4 className={Style.presentation}>{texto}</h4>) }
+        {language  === 'ES' ? <h2 className={Style.tittle}>Conocenos!</h2> : <h2 className={Style.tittle}>Get to know us!</h2>}
+        <section className={Style.container2}>
+            {language === 'ES' ? about.membersEs.map(member=> <div className={Style.containerMember}>
+                <img src={member.image} alt={`${member.name} img`} className={Style.memberImg} />
+                <h2 className={Style.memberName}>{member.name}</h2>
+                <h3 className={Style.memberWork}>{member.ocupacion}</h3>
+                <div className={Style.tittleAndText}>
+                    <h4 className={Style.subtittles}>Generos Favoritos</h4>
+                    <h5 className={Style.text2}>{member.generosFavoritos}</h5>
+                </div>
+                <div className={Style.tittleAndText}>
+                    <h4 className={Style.subtittles}>Tematicas Favoritas</h4>
+                    <h5 className={Style.text2}>{member.tematicasFavoritas}</h5>
+                </div>
+
+            </div>)
+            :about.membersEn.map(member=><div className={Style.containerMember}>
+                <img src={member.image} alt={`${member.name} img`} className={Style.memberImg} />
+                <h2 className={Style.memberName}>{member.name}</h2>
+                <h3 className={Style.memberWork}>{member.ocupacion}</h3>
+                <div className={Style.tittleAndText}>
+                    <h4 className={Style.subtittles}>Favorite Genres</h4>
+                    <h5 className={Style.text2}>{member.generosFavoritos}</h5>
+                </div>
+                <div className={Style.tittleAndText}>
+                    <h4 className={Style.subtittles}>Favorite Tematics</h4>
+                    <h5 className={Style.text2}>{member.tematicasFavoritas}</h5>
+                </div>
+            </div>)
+            }
+        </section>
     </div>)
 }
 
--- client/src/data/docs/about.js
+++ client/src/data/docs/about.js
@@ -1,5 +1,42 @@
-export const presentacionEs= ['Dos Estrellas nace como idea justo a finales de 2022 bajo las manos de dos amantes de los videojuegos obsesionados con plasmar sus disparatados conceptos, adquiridos durante muchas (créeme, muchas) horas de juego, a la realidad.',
+import provoImg from '../img/tofukaz.webp'
+import catzenrImg from '../img/raven.webp'
 
-'A día de hoy trabajamos y luchamos por sacar a delante este sueño que siempre hemos tenido, poniendo todos nuestros esfuerzos, dedicación y cariño a nuestras creaciones.']
+export const  about={ 
+    presentacionEs:['Nacemos como idea justo a finales de 2022 bajo las manos de dos amantes de los videojuegos obsesionados con plasmar sus disparatados conceptos, adquiridos durante muchas (creanme, muchas) horas de juego, a la realidad.','A día de hoy, trabajamos y luchamos por sacar adelante este sueño que siempre hemos tenido, poniendo todos nuestros esfuerzos, dedicación y cariño a nuestras creaciones.'],
+    presentacionEn:['We are born as an idea right at the end of 2022 under the hands of two video game lovers obsessed with bringing their crazy concepts, acquired through many (believe me, MANY) hours of gaming, to reality.', 'Today, we are working and fighting to bring forward this dream we have always had, putting all our efforts, dedication, and love into our creations.'],
+    membersEs:[
+                {
+                    image:catzenrImg,
+                    name:"Catzenr",
+                    ocupacion:'Diseñador web, tester',
+                    generosFavoritos: 'MMO, RPG, Mundo abierto y rogue.',
+                    tematicasFavoritas: 'Fantasia, Anime y Magia'},
 
-export const presentacionEn=['Dos Estrellas was born as an idea right at the end of 2022 under the hands of two video game lovers obsessed with bringing their crazy concepts, acquired through many (believe me, MANY) hours of gaming, to reality.', 'Today, we are working and fighting to bring forward this dream we have always had, putting all our efforts, dedication, and love into our creations.']
+                {   
+                    image:provoImg,
+                    name:"Laer",
+                    ocupacion:'Desarrollador, programador, artista pixel art y diseñador de niveles.',
+                    generosFavoritos: 'Estrategia y Gestión de recursos.',
+                    tematicasFavoritas: 'Fantasía y Ciencia Ficción.'
+                }
+            ],
+    membersEn:[
+        {
+            image:catzenrImg,
+            name:'Catzenr',
+            ocupacion:'Web developer, tester',
+            generosFavoritos:'MMO, RPG, Open world and rogue',
+            tematicasFavoritas:'Fantasy, Anime and Magic'
+        },
+        {   
+            image:provoImg,
+            name:'Laer',
+            ocupacion:'Developer, programer, pixel art artist and level designer',
+            generosFavoritos:'Strategy and gestion',
+            tematicasFavoritas:'Fantasy and Scy Fy'
+        }
+    ]
+    
+}
+
+
